248a2dc200055179e1b6efc4f3e2fb154877c140
sally5670 <144642113+sally5670@users.noreply.github.com>
2023-10-05 15:52:16 +0900

    Update main.c
    2

--- main.c
+++ main.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-  int a;
-  
-  printf("정수 하나를 입력하시오: ");
-  scanf("%d", &a);
-  
-  if(a>0)
-       printf("양수입니다.");
-  else if(a<0)
-       printf("음수입니다.");
-  else
-      printf("0입니다.");
-  
-  system("PAUSE");   
-  return 0;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+   int a; 
+   printf("정수 하나를 입력하시오: ");
+   scanf("%d", &a);
+   if(a==0)
+      printf("절대값은 %d 입니다",a);
+   else if(a>0)
+      printf("절대값은 %d 입니다.",a);
+   else
+      printf("절대값은 %d입니다.",-a);    
+   return 0;
 }
