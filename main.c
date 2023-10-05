d8917060942238cffcf8d706648e134603719dc0
sally5670 <144642113+sally5670@users.noreply.github.com>
2023-10-05 16:10:55 +0900

    3

--- main.c
+++ main.c
@@ -4,14 +4,18 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-   int a; 
-   printf("정수 하나를 입력하시오: ");
-   scanf("%d", &a);
-   if(a==0)
-      printf("절대값은 %d 입니다",a);
-   else if(a>0)
-      printf("절대값은 %d 입니다.",a);
-   else
-      printf("절대값은 %d입니다.",-a);    
+   int num=0;
+   char c;
+   
+   printf("input a string: ");
+   
+   while((c=getchar())!='\n')
+   {
+   	if(c>='0'&&c<='9')
+   		num++;
+   }
+   printf("%d",num);
+   
+   	 
    return 0;
 }
