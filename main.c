483468bb022e16c81dc57321772c84442061c1c5
sally5670 <144642113+sally5670@users.noreply.github.com>
2023-10-05 16:34:39 +0900

    6

--- main.c
+++ main.c
@@ -4,13 +4,32 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-   int i,a,sum=0;
-   printf("input a number: ");
-   scanf("%d", &a);
-   for(i=0;i<=a;i++)
-		sum+=i;
-	printf("%d",sum);
+   int number=59,count=0,a;
+   do {
+   	printf("Guess a number!");
+   	scanf("%d", &a);
+   	if(a>number)
+   	{
+		printf("high!\n");
+   		count++;
+	}
+   		
+   	else if(a<number)
+   	{
+		printf("low!\n");
+   		count++;
+	}
+   		
+   	else
+   		{
+		printf("congratulations!\n");
+   		count++;
+		printf("trials: %d",count);
+		break;
+		}
+   	
+   }while(1);
    
-   	system("PAUSE");
+	system("PAUSE");
    return 0;
 }
